f1ab7561e561848aac36d9b4304a0d94004a586b
Luke Farren <lukefarren@btinternet.com>
2018-12-16 21:04:49 +0000

    Add files via upload

---
+++ CalculatorProgramLukeFarren.cpp
@@ -0,0 +1,20 @@
+//Calculation Program made by Luke Farren - 2018
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+   int a, b, c;
+   
+   cout << "Please enter first number to calculate: ";
+   cin >> a;
+   cout << endl;
+   cout << "Please enter second number to calculate: ";
+   cin >> b;
+   cout << endl;
+   c = a + b;
+   cout <<"= " << c << endl;
+   
+   return 0;
+}
