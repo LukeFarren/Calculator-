15c7a6a0494ec2d938cdac585b2dee24c9afd0a3
Luke Farren <lukefarren@btinternet.com>
2018-12-16 21:09:13 +0000

    Update CalculatorProgramLukeFarren.cpp

--- CalculatorProgramLukeFarren.cpp
+++ CalculatorProgramLukeFarren.cpp
@@ -1,20 +1,20 @@
 //Calculation Program made by Luke Farren - 2018
+#include  <iostream>
+using  namespace std;
 
-#include <iostream>
-using namespace std;
- 
 int main()
+    
 {
-   int a, b, c;
-   
-   cout << "Please enter first number to calculate: ";
-   cin >> a;
-   cout << endl;
-   cout << "Please enter second number to calculate: ";
-   cin >> b;
-   cout << endl;
-   c = a + b;
-   cout <<"= " << c << endl;
-   
-   return 0;
+		    int a, b, c;
+		    cout << "Please enter first number to calculate: ";
+		    cin >> a; //Input integer.
+		    cout << endl; // Uncessary spacing, used for asphetics.
+		    cout << "Please enter second number to calculate: ";
+		    cin >> b; //Input integer.
+		    cout << endl;
+		    c = a + b; // Change the addition sign to multiplication (*), division (/) or subtraction (-). 
+		    cout <<"= " << c << endl; //Output Integer.
+	    
+  return  0;
 }
+    
